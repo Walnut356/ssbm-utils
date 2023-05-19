2c5e0a7cbacac28e04b9bafb827474dc8083c02c
Walnut <39544927+Walnut356@users.noreply.github.com>
2023-05-18 21:34:35 -0500

    fromat and frame data

--- src/calc.rs
+++ src/calc.rs
@@ -1,10 +1,8 @@
-
-
 use std::cmp;
 
 pub fn shield_stun(damage: &f32, analog: &f32) -> i32 {
     let a: f32 = 0.65 * (1.0 - (analog - 0.3) / 0.7);
-    return (((damage * (a + 0.3)) * 1.5 + 2.0) * (200.0/201.0)).floor() as i32;
+    return (((damage * (a + 0.3)) * 1.5 + 2.0) * (200.0 / 201.0)).floor() as i32;
 }
 
 pub fn shield_size(hp: &f32, analog: &f32) -> f32 {
@@ -21,7 +19,10 @@ pub fn hit_lag(damage: &f32, electric: Option<bool>, crouch_cancel: Option<bool>
         true => 0.666667,
         false => 1.0,
     };
-    return cmp::min(((((damage / 3.0).floor() + 3.0).floor() * e).floor() * cc).floor() as i32, 20);
+    return cmp::min(
+        ((((damage / 3.0).floor() + 3.0).floor() * e).floor() * cc).floor() as i32,
+        20,
+    );
 }
 
 pub fn shield_pushback(damage: &f32, analog: &f32) -> Vec<f32> {
@@ -29,6 +30,7 @@ pub fn shield_pushback(damage: &f32, analog: &f32) -> Vec<f32> {
     //defender
     let mut a = 0.195 * (1.0 - (analog - 0.3) / 0.7);
     let d_push = (damage * (a + 0.09) + 0.4) * 0.6; //TODO .6 for normal, 1 for power
+
     //attacker
     a = (analog - 0.3) * 0.1;
     let a_push = (damage * a) + 0.02;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use ssbm_utils::calc::*;
 fn main() {
-    let damage = 15.0;
+    let damage = 9.0;
     let analog = 1.0;
     let thing = hit_lag(&damage, Some(false), Some(false));
     let eef = shield_stun(&damage, &analog);
